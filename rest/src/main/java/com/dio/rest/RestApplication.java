d05abc32c5a33b107a6eb1d6d318cbdcbf64078c
Edmar <edmarvieira003@gmail.com>
2024-07-26 11:43:39 -0300

    spring rest

---
+++ rest/src/main/java/com/dio/rest/RestApplication.java
@@ -0,0 +1,15 @@
+package com.dio.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApplication.class, args);
+	}
+
+}
